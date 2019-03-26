037ac4852483f7a4f7a7d5438495d2d224530ed0
pengji <jipengpro@gmail.com>
2019-03-26 14:36:03 +0800

    add flag

--- packages/ReadMe.md
+++ packages/ReadMe.md
@@ -1,2 +1,10 @@
 [Packages](https://golang.org/pkg/)  
-包使用示例
+
+
+## flag
+[Package flag](https://golang.org/pkg/flag/#BoolVar)  
+两种高级命令行解析方式  
+1. [urfave/cli](https://github.com/urfave/cli)
+2. [spf13/cobra](https://github.com/spf13/cobra)
+### references
+[golang flag包使用笔记](https://www.jianshu.com/p/f9cf46a4de0e)
---
+++ packages/flag/example.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	h bool
+
+	v, V bool
+	t, T bool
+	q *bool
+
+	s string
+	p string
+	c string
+	g string
+)
+
+func init(){
+	flag.BoolVar(&h, "h", false, "help info")
+
+	flag.BoolVar(&v, "v", false,"show version")
+	flag.BoolVar(&V, "V", false,"show version and configure options")
+
+	flag.BoolVar(&t, "t", false, "test configure and exit")
+	flag.BoolVar(&T, "T", false, "test configuration, dump it and exit")
+
+	// 另一种绑定方式
+	q = flag.Bool("q", false, "supress non-error messages during configuration testing")
+
+	flag.StringVar(&s, "s", "", "send `signal` to a master process: stop, quit, reopen, reload")
+	flag.StringVar(&p, "p", "/usr/local/nginx", "set `prefix` path")
+	flag.StringVar(&c, "c", "conf/nginx.conf", "set configuration `file`")
+	flag.StringVar(&g, "g", "conf/nginx.conf", "set global `dirextives` out of configuration file")
+
+	// 改变默认的 usage, usage是一种函数类型，这里覆盖默认函数实现
+	flag.Usage = usage
+
+}
+
+func usage(){
+	fmt.Fprintf(os.Stderr, `nginx version: nginx/1.10.0
+Usage: nginx [-hvVtTq] [-s signal] [-c filename] [-p prefix] [-g directives]
+
+Options:`)
+
+	flag.PrintDefaults()
+}
+
+func main(){
+	flag.Parse()
+
+	if h {
+		flag.Usage()
+	}
+}
