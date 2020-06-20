f0a2ce536ec0b1d66edd8ad873b128693d2e86e0
pengji <jipengpro@gmail.com>
2020-06-20 12:01:03 +0800

    modify files

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # golang code
-go代码
 
 # references
 [The Go Programming Language](http://www.gopl.io/)  
 [Go Documentation](https://golang.org/doc/)  
 [Go 中文文档](https://go-zh.org/doc/)
+
 [Go语言圣经（中文版）](https://yar999.gitbooks.io/gopl-zh/content/index.html)  
 [Go语言圣经 代码](https://github.com/adonovan/gopl.io/)
+
 [GO 命令教程](https://github.com/hyper0x/go_command_tutorial)
+
+[Go by Example](https://gobyexample.com/)
+[Go 示例](https://www.kancloud.cn/itfanr/go-by-example/81681)
 
 [aQuaYi/LeetCode-in-Go](https://juejin.im/repo/5a546c286fb9a073b8d47592)
 
 # contacts
-[季朋](www.jipeng.me)  
 jiepngpro@gmail.com
--- programming_general/data_type/append.go
+++ programming_general/data_type/append.go
@@ -2,18 +2,18 @@ package main
 
 import "fmt"
 
-func appendslice(x []int, y ...int) []int{
-	var z[]int
+func appendslice(x []int, y ...int) []int {
+	var z []int
 	zlen := len(x) + len(y)
-	if zlen <= cap(x){
+	if zlen <= cap(x) {
 		// there is room to expand the slice
 		z = x[:zlen]
-	}else{
+	} else {
 		// there is insufficient space
 		// grow by doubling, for amortized linear complexity
 		zcap := zlen
-		if zcap < 2*len(x){
-			zcap = 2*len(x)
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
 		}
 		z = make([]int, zlen, zcap)
 		copy(z, x)
@@ -22,18 +22,18 @@ func appendslice(x []int, y ...int) []int{
 	return z
 }
 
-func appendInt(x []int, y int) []int{
-	var z[]int
-	zlen := len(x)+1
-	if zlen <= cap(x){
+func appendInt(x []int, y int) []int {
+	var z []int
+	zlen := len(x) + 1
+	if zlen <= cap(x) {
 		// there is room to grow, extend the slice
 		z = x[:zlen]
-	}else{
+	} else {
 		// there is insufficient space, allocate a new array
 		// grow by doubling, for amortized linear complexity.
 		zcap := zlen
-		if zcap < 2*len(x){
-			zcap = 2*len(x)
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
 		}
 		z = make([]int, zlen, zcap)
 		copy(z, x)
@@ -42,11 +42,14 @@ func appendInt(x []int, y int) []int{
 	return z
 }
 
-func main(){
+func main() {
 	var x, y []int
-	for i:=0; i<10; i++{
+	for i := 0; i < 10; i++ {
 		y = appendInt(x, i)
 		fmt.Printf("%d cap=%d\t%v\n", i, cap(y), y)
 		x = y
 	}
 }
+
+// Golang 切片与函数参数“陷阱”
+// https://studygolang.com/articles/9876
